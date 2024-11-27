9f84dd619c55d5adaefb2fcd52eed5445b4d32ef
poojangit <poojang147@gmail.com>
2024-11-27 12:52:28 +0530

    non-primitive datatypes

--- DataTypes/dataTypes.js
+++ DataTypes/dataTypes.js
@@ -2,68 +2,117 @@
 
 //^ 1. number -----------------------
 
-num1 = 55
-num2 = 45.89
-num3 = NaN
+// num1 = 55
+// num2 = 45.89
+// num3 = NaN
 
-console.log(typeof num1);
-console.log(typeof num2);
-console.log(typeof num3);
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof num3);
 
 //^ 2. string ------------------------
 
-str1 = "Pooja"
-str2 = 'hi'
-age = 14
-str3 = `I am ${age} old`
+// str1 = "Pooja"
+// str2 = 'hi'
+// age = 14
+// str3 = `I am ${age} old`
 
-console.log(typeof str1);
-console.log(typeof str2);
-console.log(typeof str3);
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(typeof str3);
 
 //^ 3. boolean -----------------------
 
-isAdult = true
-isMarried = false
+// isAdult = true
+// isMarried = false
 
-console.log(typeof isAdult);
-console.log(typeof isMarried);
+// console.log(typeof isAdult);
+// console.log(typeof isMarried);
 
 //^ 4. null --------------------------
 
-let pincode = null
-console.log(typeof pincode); // typeof null is Object
+// let pincode = null
+// console.log(typeof pincode); // typeof null is Object
 
 //^ 5. undefined --------------------
 
-let phoneno 
-let parentname = undefined
-console.log(typeof phoneno);
-console.log(typeof parentname );
+// let phoneno 
+// let parentname = undefined
+// console.log(typeof phoneno);
+// console.log(typeof parentname );
 
 //^ 6. Symbol-------------------------
 
-let uniqueId = Symbol("id")
-let uniqueValue = Symbol("id")
-console.log( typeof uniqueId);
-console.log(typeof uniqueValue);
-console.log(uniqueId);
-console.log(uniqueValue);
+// let uniqueId = Symbol("id")
+// let uniqueValue = Symbol("id")
+// console.log( typeof uniqueId);
+// console.log(typeof uniqueValue);
+// console.log(uniqueId);
+// console.log(uniqueValue);
 
-console.log(uniqueId == uniqueValue);
-console.log(uniqueId === uniqueValue);
+// console.log(uniqueId == uniqueValue);
+// console.log(uniqueId === uniqueValue);
 
 //^ 7. bigint -----------------------
-let largeNum = 12221212n
-console.log(typeof largeNum);
+// let largeNum = 12221212n
+// console.log(typeof largeNum);
 
 // !------------------------------------------------------------------------------
 
-let n1 = "1"
-let n2 = 1
-console.log(n1==n2);
-console.log(n1===n2);
+// let n1 = "1"
+// let n2 = 1
+// console.log(n1==n2);
+// console.log(n1===n2);
 
+
+// todo ----------NON - Primitive datatypes---------------------
+
+//^ 1.Objects(collection of key and value pair) -------------
+
+// let person = {
+//     pname : "Riya",
+//     age : 26,
+//     greet : function () {
+//         console.log("Hello");
+//     }
+// }
+// console.log(person.pname);
+// console.log(person.greet());
+// person.greet()
+
+// ^ 2. Arrays ( list -like objects used to store multiple values)-----
+
+// let arr1 = ["1", 2, "Js"]
+// console.log(arr1);
+// console.log(arr1[1]);
+// arr1[1] = "hi"
+// console.log(arr1[1]);
+// console.log(arr1);
+
+// ^ 3. Function - These are the callable objects-----
+
+// function add(a,b) {
+//     return a+ b
+// }
+// console.log(add(1,2));
+
+// ^ 4. Date ----------------------------------------
+// let date = new Date()
+// console.log(date);
+
+// ^ 5. Set -----------------------------------------
+
+// let Int = new Set([1,2,3,4])
+// console.log(Int);
+// console.log(Int.entries());
+// console.log(Int.size);
+// console.log(Int.keys())
+
+// ^ 6. Map ------------------------------------------
+// let userMapping = new Map()
+// userMapping.set("name", "Pachuu")
+// userMapping.set(1,"One")
+// console.log(userMapping.get("name"));
 
 
 
