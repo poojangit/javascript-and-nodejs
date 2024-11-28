a65b8b7b355c3a2e78aec0791317bd8aa37e94b1
poojangit <poojang147@gmail.com>
2024-11-28 21:05:59 +0530

    [POOJA] Examples : practiced with 3 examples of callback on synchronous, asynchronous and using event handling

--- Callbacks/callbacks.js
+++ Callbacks/callbacks.js
@@ -4,11 +4,49 @@
 //^ 2. It is used for handling the asynchronous operations such as fetching dta from an API or reading files.
 
 // Basic example of callback------------>
-function product(laptop){
-    console.log("Im inside product");
-    laptop()
-}
-function myLaptop() {
-    console.log("This is my laptop");
-}
-product(myLaptop)
+// function product(laptop){
+//     console.log("Im inside product");
+//     laptop()
+// }
+// function myLaptop() {
+//     console.log("This is my laptop");
+// }
+// product(myLaptop)
+
+//!Examples
+
+//^ 1. Synchronous callbacks ----------->
+
+// function order(name, callback){
+//     console.log(`Order a ${name}` );
+//     callback()
+// }
+// order("chocolate" , function(){
+//     console.log("Ordered successfully");
+// })
+
+//^ 2. Asynchronous callbacks ---------->
+
+// function timeManagement(callback){
+//     setTimeout(() => {
+//         callback()
+//     }, 5000);
+// }
+// timeManagement(()=>{
+//     console.log("Time up");
+// })
+
+// function speek(callback) {
+//     setTimeout(function() {
+//         callback()
+//     },2000)
+// }
+// speek(function() {
+//     console.log("You can start to speak");
+// })
+
+//^ 3. Using Event handling
+
+// document.getElementById("btn").addEventListener("click", function() {
+//     console.log("Did you just click?");
+// })
