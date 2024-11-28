0d0613bfdde05d3ce5e2ad6ea18518b0f8615ff3
poojangit <poojang147@gmail.com>
2024-11-28 11:18:21 +0530

    Hoisting in Js

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,33 @@
+// todo ------------HOISTING----------------
+
+// var n = 5
+// function normalFunc(){
+//     console.log("Hello Good morning");
+// }
+// normalFunc()
+// console.log(n);
+
+// console.log(normalFunc);
+// normalFunc()
+// console.log(n); //undefined
+// var n = 5
+// function normalFunc(){
+//     console.log("Hello Good morning");
+// }
+
+
+// arrowFunc()
+// console.log(n); //undefined
+// var n = 5
+// var arrowFunc = () => {
+//     console.log("Hello Good morning");
+// }
+
+// var x = 7
+// function getName() {
+//     console.log("Hello Javascript");
+// }
+// getName()
+// console.log(x);
+// console.log(getName);
+
