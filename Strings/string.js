2ddb162e37e805ea41b359af8f5f1f98375e67ef
poojangit <poojang147@gmail.com>
2024-11-29 18:10:24 +0530

    [POOJA] String: Way of string creation

---
+++ Strings/string.js
@@ -0,0 +1,22 @@
+//todo ---------------String in Js----------------------
+
+//^ Creating string ---------------------->
+
+//! single quote
+
+let productName = 'Mobile'
+console.log(productName);
+
+//! double quote
+
+let bookName = "Beauty and the beast"
+console.log(bookName);
+
+//! template literals
+
+let age = 20
+console.log(`My age is ${age}`);
+
+//^ String methods ----------------------->
+
+
