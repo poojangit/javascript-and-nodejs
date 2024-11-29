03e3e3f5c49c44f8471760efa774ddbbae3716fc
poojangit <poojang147@gmail.com>
2024-11-29 18:24:00 +0530

    [POOJA] String : Various methods in string

--- Strings/string.js
+++ Strings/string.js
@@ -4,19 +4,61 @@
 
 //! single quote
 
-let productName = 'Mobile'
-console.log(productName);
+// let productName = 'Mobile'
+// console.log(productName);
 
 //! double quote
 
-let bookName = "Beauty and the beast"
-console.log(bookName);
+// let bookName = "Beauty and the beast"
+// console.log(bookName);
 
 //! template literals
 
-let age = 20
-console.log(`My age is ${age}`);
+// let age = 20
+// console.log(`My age is ${age}`);
 
 //^ String methods ----------------------->
+
+//! to return a length of a string
+let storyName = 'Life of pie IT'
+let bookName = " IT ENDS WITH US "
+console.log(storyName.length);
+
+console.log(storyName.charCodeAt(1));
+
+console.log(storyName.substring(1,2));
+
+console.log(bookName.toLowerCase());
+
+console.log(storyName.toUpperCase());
+
+console.log(bookName.toLocaleLowerCase());
+
+console.log(bookName.toLocaleUpperCase());
+
+console.log(bookName.endsWith("S")); //returns boolean
+
+console.log(bookName.concat(storyName));
+
+console.log(bookName.charAt(1)); //return character
+
+console.log(bookName.match(storyName));
+
+console.log(bookName.slice(1,4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
