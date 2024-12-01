8f9a2c535e6763220dac3b49ea383dcedf0ba1e2
poojangit <poojang147@gmail.com>
2024-12-01 23:54:03 +0530

    [POOJA] examples : Looked into some more examples on closures

--- Closures/closures.js
+++ Closures/closures.js
@@ -5,10 +5,80 @@
 
 //~ Example 1
 
-function outerFunction(outerVariable){
-    return function innerFunction(innervariable){
-        console.log(`Outer : ${outerVariable} Inner : ${innervariable}`);
+// function outerFunction(outerVariable){
+//     return function innerFunction(innervariable){
+//         console.log(`Outer : ${outerVariable} Inner : ${innervariable}`);
+//     }
+// }
+// const closureFunction = outerFunction("Hello")
+// closureFunction("world")
+
+//~ Example 2
+
+// function developer() {
+//     var name = 'pooja'
+//     function displayName() {
+//         console.log(name);
+//     }
+//     return displayName
+// }
+// var devName = developer()
+// devName()
+
+//~ Example 3
+
+// function x() {
+//     var a = 7
+//     function y() {
+//         console.log(a);
+//     }
+//     y()
+// }
+// x()
+
+//~ Example 4
+
+// function x() {
+//     var a = 7
+//     function y() {
+//         console.log(a);
+//     }
+//     return y
+// }
+// var z = x()
+// console.log(z);
+// z()
+
+//~ Example 5
+// function x() {
+//     var a = 7
+//     function y() {
+//         console.log(a);
+//     }
+//     a = 100
+//     return y
+// }
+// var z = x()
+// console.log(z);
+// z()
+
+//~ Example 6
+
+function z() {
+    var b = 1000
+    function x() {
+        var a = 7
+        function y() {
+            console.log(a, b);
+        }
+        y()
     }
+    x()
 }
-const closureFunction = outerFunction("Hello")
-closureFunction("world")
+z()
+
+//^ Uses of closures
+
+//! Functions like once
+//! memoize
+//! setTimeouts
