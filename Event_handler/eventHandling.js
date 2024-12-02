7124672afc1f92aba59fce0ad32f4421248725b1
poojangit <poojang147@gmail.com>
2024-12-02 12:12:11 +0530

    [POOJA] Added : Defination of Event handling in js

---
+++ Event_handler/eventHandling.js
@@ -0,0 +1,4 @@
+//todo -------------EVENT HANDLING---------------------
+
+//^ It is a process of resposnding to the users interation or events like clicks, key presses, mouse movements , form submissions etc.
+
