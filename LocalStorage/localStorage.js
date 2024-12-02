5ba241e4907076d43b930db1e9bc1e8f45667fcb
poojangit <poojang147@gmail.com>
2024-12-02 13:06:07 +0530

    [POOJA] Defination and key features

---
+++ LocalStorage/localStorage.js
@@ -0,0 +1,11 @@
+//todo --------------LOCAL STORAGE-----------------
+
+//^ Local storage is a web storage API that is provided by browsers that allows us to store data persistently in the browser
+
+//& Key features
+
+//! Persistant storage
+//! key-value pair
+//! synchronous API
+//! size limit 
+
