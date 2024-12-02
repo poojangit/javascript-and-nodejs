7da26481168a782ec12815d897334260a17fe1fe
poojangit <poojang147@gmail.com>
2024-12-02 10:38:04 +0530

    [POOJA] escape : examples on escape function and a new version of it

---
+++ Escape_Unescape/escape_unscape.js
@@ -0,0 +1,14 @@
+//todo --------ESCAPE() and UNSCAPE()------------------
+
+//! Escape() ----------
+
+const data = "my password"
+console.log(escape(data));
+
+
+const password = "pooja hello how"
+console.log(encodeURI(password));
+
+const encode = "Good morning"
+console.log(encodeURIComponent(encode));
+
