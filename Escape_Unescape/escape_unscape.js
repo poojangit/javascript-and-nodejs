76c0766bc59c5c67201c7a373ee480a7790324e7
poojangit <poojang147@gmail.com>
2024-12-02 10:43:47 +0530

    [POOJA] unscape : examples on unscape function and new version of it

--- Escape_Unescape/escape_unscape.js
+++ Escape_Unescape/escape_unscape.js
@@ -12,3 +12,15 @@ console.log(encodeURI(password));
 const encode = "Good morning"
 console.log(encodeURIComponent(encode));
 
+//! unscape() -------------
+
+
+const data1 = "my%20password"
+console.log(unescape(data1));
+
+
+const password1 = "pooja%20hello%20how"
+console.log(decodeURI(password1));
+
+const encode1 = "Good%20morning"
+console.log(decodeURIComponent(encode1));
