a73917ef3172706a9e3ad74b6b21680ff3635bb5
poojangit <poojang147@gmail.com>
2024-12-02 20:08:50 +0530

    [POOJA] HOF : two more examples on HOF

--- HOF/higherOrder.js
+++ HOF/higherOrder.js
@@ -4,15 +4,79 @@
 
 //~ Example 1 ------------------
 
-function greet(name){
-    return function(message){
-        console.log(`Hello ${name} ${message}`);
-    }
-}
-const greetWithName = greet("Pooja")
-greetWithName("Good Morning")
+// function greet(name){
+//     return function(message){
+//         console.log(`Hello ${name} ${message}`);
+//     }
+// }
+// const greetWithName = greet("Pooja")
+// greetWithName("Good Morning")
 
 //~ Example 2 ------------------
 
+// function x(){
+//     console.log("Hello");
+// }
+// function y(x){
+//     x()
+// }
 
+//~ Example 3 
+
+//! Basic logic
+
+// const radius = [3,1,2,4]
+// const calcArea = function (radius) {
+//     const output = []
+//     for(let i=0; i<radius.length;i++){
+//         output.push(Math.PI * radius[i] * radius[i])
+//     }
+//     return output
+// }
+// console.log(calcArea(radius));
+
+// const calcCircumferance = function (radius) {
+//     const output = []
+//     for(let i=0; i<radius.length;i++){
+//         output.push(2*(Math.PI * radius[i]))
+//     }
+//     return output
+// }
+// console.log(calcCircumferance(radius));
+
+// const calcDiameter = function (radius) {
+//     const output = []
+//     for(let i=0; i<radius.length;i++){
+//         output.push(2*radius[i])
+//     }
+//     return output
+// }
+// console.log(calcDiameter(radius));
+
+//! using hof
+
+const radius = [3,1,2,4]
+
+const area = function(radius){
+    return Math.PI*radius*radius
+}
+const circumferance = function(radius){
+    return 2*Math.PI*radius
+}
+const diameter = function(radius){
+    return Math.PI*radius
+}
+
+// const calculate = function (radius, logic){
+//     const output = []
+//     for(let i=0; i<radius.length;i++){
+//         output.push(logic(radius[i]))
+//     }
+//     return output
+// }
+// console.log(calculate(radius,area));
+// console.log(calculate(radius, circumferance));
+// console.log(calculate(radius,diameter));
+
+console.log(radius.map(area));
 
