fc0e7243625e8347831a06e88188202c9b3a3e5e
poojangit <poojang147@gmail.com>
2024-12-02 12:54:05 +0530

    [POOJA] Event : Added some of the events

--- Event_handler/eventHandling.js
+++ Event_handler/eventHandling.js
@@ -2,4 +2,24 @@
 
 // ^ It is a process of resposnding to the users interation or events like clicks, key presses, mouse movements , form submissions etc.
 
+document.getElementById("submit").addEventListener("click", function(){
+    console.log("You have submitted the form"); 
+})
 
+//! click - button click
+//! mousehover - hover effect
+//! keydown - typeing in a input field
+//! submit - form submission
+//! load - window or image load
+
+// document.getElementById("input").addEventListener('keyup', function() {
+//     console.log("You stoped writting");
+// })
+
+document.getElementById("input").addEventListener('keydown', function(event) {
+    console.log("You started writing" , event.key);
+})
+
+// document.getElementById("input").addEventListener('keypress', function() {
+//     console.log("start");
+// })
