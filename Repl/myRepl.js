881bdc284e5fa74cb394018e58159171ceca0229
poojangit <poojang147@gmail.com>
2024-12-03 11:09:14 +0530

    [POOJA] REPL : Defination and execution file added

---
+++ Repl/myRepl.js
@@ -0,0 +1,10 @@
+const sum = (a,b) => a+b
+sum(1,2)
+console.log("Hello REPL")
+const greet = (name) => {
+return `Hello, ${name}`
+}
+
+console.log(greet)
+greet()
+greet("Pooja")
---
+++ Repl/repl.js
@@ -0,0 +1,6 @@
+//TODo -------------REPL-----------------------------
+
+//^ REPL is a interactive environment that takes single user inputs(Read),
+//^ evaluates them (eval)
+//^ Prints the result(Print)
+//^ then loops back to accept more inputs(Loop)
