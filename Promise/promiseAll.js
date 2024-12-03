d7b096bc3f0bc6ed93bf58dddac4fdce1b1f8bb7
poojangit <poojang147@gmail.com>
2024-12-03 12:27:08 +0530

    [POOJA] Promise all methods : understanding and use of promise.all() in js and its implementation

--- Promise/promiseAll.js
+++ Promise/promiseAll.js
@@ -4,27 +4,39 @@
 
 //~ Example 1
 
-const promise1 = Promise.resolve(10)
-const promise2 = Promise.resolve(20)
-const promise3 = Promise.resolve(30)
-const promise4 = Promise.resolve(40)
+// const promise1 = Promise.resolve(10)
+// const promise2 = Promise.resolve(20)
+// const promise3 = Promise.resolve(30)
+// const promise4 = Promise.resolve(40)
 
-const promiseResult = Promise.all([promise1,promise2,promise3,promise4])
-promiseResult.then((value) => console.log(value)
-).catch((error)=> console.log(error)
-)
+// const promiseResult = Promise.all([promise1,promise2,promise3,promise4])
+// promiseResult.then((value) => console.log(value)
+// ).catch((error)=> console.log(error)
+// )
 
 //~ Example 2
 
 
 // const promise1 = Promise.resolve(10)
 // const promise2 = Promise.resolve(20)
-// const promise3 = Promise.reject(30)console.log(promiseResult);
+// const promise3 = Promise.reject(30)
 // const promise4 = Promise.resolve(40)
 
 // const promiseResult = Promise.all([promise1,promise2,promise3,promise4])
 // console.log(promiseResult);
 // promiseResult.then((values) => console.log(values)).catch((error) => console.log(error))
+
+//~ Example 3
+
+const promise1 = Promise.resolve(10)
+const promise2 = Promise.resolve(20)
+const promise3 = Promise.reject(30)
+const promise4 = Promise.reject(40)
+
+const promiseResult = Promise.all([promise1,promise2,promise3,promise4])
+console.log(promiseResult);
+promiseResult.then((values) => console.log(values)).catch((error) => console.log(error))
+
 
 
 
