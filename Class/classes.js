6584c068a7109d48fd3c495228faaa3ad4d7ed5f
poojangit <poojang147@gmail.com>
2024-12-07 16:00:49 +0530

    Update classes.js and index.html

--- Class/classes.js
+++ Class/classes.js
@@ -33,21 +33,91 @@
 
 //~ Example 3
 
-class AudiCar {
-    start() {
-        console.log("start")
+// class AudiCar {
+//     start() {
+//         console.log("start")
+//     }
+//     stop() {
+//         console.log("stop")
+//     }
+//     setBrand(brand) {
+//         this.brand = brand
+//     }
+// }
+// console.log(AudiCar);
+// let fortuner = new AudiCar()
+// fortuner.setBrand = "fortuner"
+// let lexus = new AudiCar()
+// lexus.setBrand("lexus")
+// lexus.start()
+// lexus.stop()
+
+//~ Example 4
+//! adding methods
+
+// class Reac{
+//     width = 0
+//     height = 0
+//     constructor(width, height){
+//         this.width = width
+//         this.height = height
+//     }
+//     redct() {
+//         return this.width * this.height
+//     }
+// }
+// const react1 = new Reac(4,4)
+// console.log(react1.redct());
+
+//~ Example 5
+//^ getters and setters
+
+// class Person {
+//     constructor(name){
+//         this._name = name
+//     }
+
+//     get name() {
+//         return this._name
+//     }
+//     set name(newName) {
+//         if(newName.length > 0){
+//             this._name = newName
+//         }
+//         else {
+//             console.log("Name must be not empty");
+//         }
+//     }
+// }
+// const person = new Person("Pachu")
+// console.log(person.name);
+// person.name = "he"
+// console.log(person.name);
+
+//~ Example 6 
+//^ Inheritance
+//! classes can inherit from other classes using the extends keyword
+//! This allows class to reuse methods and properties from the parent class
+
+class Animal{
+    constructor(name){
+        this.name = name
     }
-    stop() {
-        console.log("stop")
-    }
-    setBrand(brand) {
-        this.brand = brand
+    eat() {
+        console.log(`${this.name} is eating`);
     }
 }
-console.log(AudiCar);
-let fortuner = new AudiCar()
-fortuner.setBrand = "fortuner"
-let lexus = new AudiCar()
-lexus.setBrand("lexus")
+class Dog extends Animal{
+    bark(){
+        console.log(`${this.name} is barking`);  
+    }
+}
+const dog = new Dog("Tuffy")
+dog.bark()
+dog.eat()
+
+
+
+
 
 
